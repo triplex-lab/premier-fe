ba50880901960a4bfc2a1631fca3bd6b0260f546
asterios_fm <asteriosfm@gmail.com>
2021-08-07 07:43:43 +0300

    last version

---
+++ src/containers/team/BinaryTree.js
@@ -0,0 +1,76 @@
+export class ReferalNode {
+    constructor(id, data, childrens=[]) {
+      this.id = id;
+      this.data = data;
+      this.childrens = childrens.length ? childrens : [];
+    }
+  }
+  
+  export class BinaryTree {
+    constructor(data) {
+      this.data = data;
+      this.middleIndex = data && Math.floor(data.length / 2);
+    }
+  
+    static convertToNodes = (array) => {
+      let nodes = array.map(node => {
+        if (node.childrens) {
+          return new ReferalNode(
+            node.id,
+            {
+              email: node.email,
+              info: node.info,
+            },
+            [...BinaryTree.convertToNodes(node.childrens)]);
+        }
+        return new ReferalNode(
+          node.id,
+          {
+            email: node.email,
+            info: node.info,
+          },
+          []
+        );
+      })
+      return nodes;
+    }
+  
+    static updater(id, array, data) {
+      let found = false;
+      if (!array || !data) {
+        return;
+      }
+      if (found) {
+        return;
+      }
+      array.forEach(node => {
+        console.log(node.id, id)
+        if (node.id === id) {
+          found = true;
+          node.childrens = data;
+          return;
+        } else {
+          this.updater(id, node.childrens, data);
+        }
+      })
+      return array;
+    }
+  
+    left() {
+      if (!this.data.length) {
+        return [];
+      } else {
+        let left = this.data.slice(0, this.middleIndex);
+        return BinaryTree.convertToNodes(left);
+      }
+    }
+  
+    right() {
+      if (!this.data.length) {
+        return [];
+      } else {
+        let right = this.data.slice(this.middleIndex, this.data.length);
+        return BinaryTree.convertToNodes(right);
+      }
+    }
+  }
---
+++ src/containers/team/ReferalsBinaryTree.js
@@ -0,0 +1,80 @@
+
+import React from 'react';
+import axios from 'axios';
+
+import s from './Team.Module.css'
+import Referal from './Referal';
+import { BinaryTree } from './BinaryTree';
+
+
+export default class ReferalsBinaryTree extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      referals: [],
+    }
+  }
+
+  getReferalsByUserId = async () => {
+    const user = await axios
+    .get("/api/user")
+    .then(response => response.data)
+    .catch(error => {
+      console.log(error)
+      return null
+    })
+    if (user) {
+      const res = await axios.get(`/team`)
+      .then(response => {
+        return response.data;
+      })
+      if (!this.state.referals.length && res.length) {
+        this.setState({
+          referals: [...res.map(referal => {
+            return {
+              ...referal,
+              childrens: [],
+            }
+          })],
+        });
+      }
+      return res;
+    }
+  }
+
+  componentDidMount() {
+    this.getReferalsByUserId();
+  }
+
+  render() {
+    const { referals } = this.state;
+    const binaryTree = new BinaryTree([...referals]);
+
+    return (
+      <div className={s.referalsRow}>
+          {binaryTree.left && binaryTree.left().map(referalNode => {
+            return <Referal
+              referal={referalNode.data}
+              referals={referals}
+              setReferals={(newState) => this.setState({...newState})}
+              childrens={referalNode.childrens}
+              id={referalNode.id}
+              key={referalNode.id}
+            />
+          })}
+          {binaryTree.right && binaryTree.right().map(referalNode => {
+            return <Referal
+              referal={referalNode.data}
+              referals={referals}
+              setReferals={(newState) => this.setState({...newState})}
+              childrens={referalNode.childrens}
+              id={referalNode.id}
+              key={referalNode.id}
+            />
+          })}
+      </div>
+    )
+  }
+}
